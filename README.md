ad1755b844f80b4bd3a4635b36f086d0d3b7187d
root <root@fv-az127-729.q1olyzzjlwvu1effj4xltge3jb.bx.internal.cloudapp.net>
2021-03-24 12:23:55 +0000

    Add extras for full_oppo6769-user 10 QP1A.190711.020 d54e398b2564fe22 release-keys
    Signed-off-by: root <root@fv-az127-729.q1olyzzjlwvu1effj4xltge3jb.bx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## full_oppo6769-user 10 QP1A.190711.020 d54e398b2564fe22 release-keys
+- Manufacturer: realme
+- Platform: mt6768
+- Codename: RMX3171
+- Brand: realme
+- Flavor: full_oppo6769-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: d54e398b2564fe22
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: RMX3171_11.A.13_0130_202103090105
+- Branch: RMX3171_11.A.13_0130_202103090105
+- Repo: realme_rmx3171_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat oppo_product/app/Photos/Photos.apk.* 2>/dev/null >> oppo_product/app/Photos/Photos.apk
+rm -f oppo_product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/Maps/Maps.apk.* 2>/dev/null >> system/system/product/app/Maps/Maps.apk
+rm -f system/system/product/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/reserve/Photos.apk.* 2>/dev/null >> system/system/reserve/Photos.apk
+rm -f system/system/reserve/Photos.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/priv-app/Settings/Settings.apk
+rm -f system/system/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk.* 2>/dev/null >> system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk
+rm -f system/system/preload/com.google.android.apps.photos-9/com.google.android.apps.photos-9.apk.* 2>/dev/null
+cat system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null >> system/system/preload/OppoVideoEditor/OppoVideoEditor.apk
+rm -f system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null >> system/system/app/GoogleLatinInput/GoogleLatinInput.apk
+rm -f system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null
